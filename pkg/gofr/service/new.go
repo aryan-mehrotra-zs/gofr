2db8fd54c816931cfd06dac9cb4862c9ecd2290a
vipul-rawat-zs <vipul.rawat@zopsmart.com>
2024-01-11 22:17:27 +0530

    add Post, PostWithHeaders and GetWithHeaders methods for http service

--- pkg/gofr/service/new.go
+++ pkg/gofr/service/new.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"bytes"
 	"context"
 	"fmt"
 	"net/http"
@@ -18,15 +19,45 @@ type httpService struct {
 }
 
 func (h *httpService) Get(ctx context.Context, path string, params map[string]interface{}) (*http.Response, error) {
+	return h.GetWithHeaders(ctx, path, params, nil)
+}
+func (h *httpService) GetWithHeaders(ctx context.Context, path string, params map[string]interface{}, headers map[string]string) (*http.Response, error) {
 	uri := h.url + "/" + path
 
-	tr := otel.Tracer("gofr-http-client")
+	tr := otel.Tracer("gofr-http-client-get")
 
 	ctx, span := tr.Start(ctx, uri)
 	defer span.End()
 
 	ctx = httptrace.WithClientTrace(ctx, otelhttptrace.NewClientTrace(ctx))
 	req, _ := http.NewRequestWithContext(ctx, "GET", uri, http.NoBody)
+
+	addHeadersToRequest(req, headers)
+	encodeQueryParameters(req, params)
+
+	return h.Do(req)
+}
+
+func (h *httpService) Post(ctx context.Context, path string, params map[string]interface{}, body []byte) (*http.Response, error) {
+	return h.PostWithHeaders(ctx, path, params, body, nil)
+}
+
+func (h *httpService) PostWithHeaders(ctx context.Context, path string, params map[string]interface{}, body []byte, headers map[string]string) (*http.Response, error) {
+	uri := h.url + "/" + path
+
+	if path == "" {
+		uri = h.url
+	}
+
+	tr := otel.Tracer("gofr-http-client-post")
+
+	ctx, span := tr.Start(ctx, uri)
+	defer span.End()
+
+	ctx = httptrace.WithClientTrace(ctx, otelhttptrace.NewClientTrace(ctx))
+	req, _ := http.NewRequestWithContext(ctx, http.MethodPost, uri, bytes.NewBuffer(body))
+
+	addHeadersToRequest(req, headers)
 	encodeQueryParameters(req, params)
 
 	return h.Do(req)
@@ -34,6 +65,9 @@ func (h *httpService) Get(ctx context.Context, path string, params map[string]in
 
 type HTTP interface {
 	Get(ctx context.Context, api string, params map[string]interface{}) (*http.Response, error)
+	GetWithHeaders(ctx context.Context, api string, params map[string]interface{}, headers map[string]string) (*http.Response, error)
+	Post(ctx context.Context, api string, params map[string]interface{}, body []byte) (*http.Response, error)
+	PostWithHeaders(ctx context.Context, api string, params map[string]interface{}, body []byte, header map[string]string) (*http.Response, error)
 }
 
 func NewHTTPService(serviceAddress string) HTTP {
@@ -58,4 +92,10 @@ func encodeQueryParameters(req *http.Request, params map[string]interface{}) {
 	}
 
 	req.URL.RawQuery = q.Encode()
+}
+
+func addHeadersToRequest(req *http.Request, headers map[string]string) {
+	for k, v := range headers {
+		req.Header.Set(k, v)
+	}
 }
